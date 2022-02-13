0c9561690803d52fcb40564b2a86c5225578098b
Zagarazhi <fukuroOwl@yandex.ru>
2022-02-13 14:49:03 +0400

    YBlock debug

--- src/main/java/ru/zagarazhi/blocks/YBlock.java
+++ src/main/java/ru/zagarazhi/blocks/YBlock.java
@@ -3,17 +3,15 @@ package ru.zagarazhi.blocks;
 import ru.zagarazhi.Answear;
 
 public class YBlock implements Block {
-    private Answear answear;
+    private Answear answear = Answear.getInstance();
     private String text;
     private Block next;
 
-    public YBlock(Answear answear, String text) {
-        this.answear = answear;
+    public YBlock(String text) {
         this.text = text;
     }
 
-    public YBlock(Answear answear, String text, Block next) {
-        this.answear = answear;
+    public YBlock(String text, Block next) {
         this.text = text;
         this.next = next;
     }
