2311ac8f905c025de4373c329e51edf7a06b0adb
Zagarazhi <fukuroOwl@yandex.ru>
2022-02-13 18:21:18 +0400

    discard app

--- src/main/java/ru/zagarazhi/App.java
+++ src/main/java/ru/zagarazhi/App.java
@@ -12,5 +12,19 @@ public class App {
     public static void main(String[] args) {
         CUI cui = new CUI();
         cui.show();
+        /*
+        List<Token> list = LexelAnalyzer.lex("YНD1Y1X1U1Y2YK");
+        ErrorObserver errorObserver = ErrorObserver.getInstance();
+        for (Token t : list) {
+            System.out.println(t.toString());
+
+        }
+        ArrayList<Block> blocks = Token.tokenListToBlockList(list.toArray(new Token[list.size()]));
+        Processor processor = new Processor(blocks);
+        List<String> errors = errorObserver.getLexicalErrors();
+        for (String error : errors) {
+            System.out.println(error);
+        }
+        */
     }
 }
